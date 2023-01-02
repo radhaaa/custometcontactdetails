a15eb8405441d9a8a3072945119eb2a0d76ebce5
unknown <anuradha.gude@gmail.com>
2023-01-02 12:45:48 +0530

    contactdata

---
+++ src/components/ContactData.js
@@ -0,0 +1,52 @@
+function ContactData(){
+    const person = [
+        {
+            id: 1,
+            name: "anu",
+            mobile: "2449999",
+            gender: "Female",
+            contacttype: "Personal"
+
+        },
+        {
+            id: 2,
+            name: "radha",
+            mobile: "33333333",
+            gender: "Female",
+            contacttype: "Business"
+        },
+        {
+            id: 3,
+            name: "abcd",
+            mobile: "88889999",
+            gender: "Male",
+            contacttype: "Personal"
+        },
+        {
+            id: 4,
+            name: "anubdhh",
+            mobile: "66649999",
+            gender: "Male",
+            contacttype: "Business"
+        }
+
+    ]
+ let datalen = [];
+ for (let x in person) {
+    
+    console.log(x)
+    console.log("hi")
+    console.log(person[x].gender) 
+  
+    console.log(datalen.length)
+    if(person[x].gender=="Male"){
+        datalen.push(person[x]);
+        console.log(person[x].name)
+    }
+    else console.log("person not found")
+ }
+
+ return(datalen.length)
+}
+
+ export default ContactData
