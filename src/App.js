d4749688259ead62870dfe2882fbabd09f5ee6e3
unknown <anuradha.gude@gmail.com>
2023-01-02 12:52:18 +0530

    fifthcommit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,10 @@ import './components/ContactData';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          welcome reactjs
+      
+        Hello Anuradha...................
 
-        </a>
-      </header>
+        
     </div>
   );
 }
